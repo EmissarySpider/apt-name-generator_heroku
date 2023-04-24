64666a8a53bde5c2a28a65f24398d036deac5cbb
Alex Delamotte <93060095+EmissarySpider@users.noreply.github.com>
2023-04-24 09:57:52 -0700

    Update app.py
    Microsoft had to change their naming convention so I suppose I am now including these new, absurd names. I also added older names referenced in their post when they were funny enough (looking at you, H0lyGh0st). https://learn.microsoft.com/en-us/microsoft-365/security/intelligence/microsoft-threat-actor-naming
    Also, added LAPSUS$ and GandCrab. Self-named actors are actors, too.

--- app/app.py
+++ app/app.py
@@ -8,31 +8,42 @@ app = Flask(__name__)
 
 
 def makeAPTName():
-    class Adjective: # organization is important for CTI professionals
-        cnAdj = ['Comment', 'Putter', 'Wicked', 'Gothic', 'Emissary', 'Golden', 'Aurora', 'Dynamite', 'Turbine', 'Vixen', 'Sneaky', 'Wet', 'Pitty', 'Strong', 'Deep', 'Cactus', 'Shadow', 'Royal', 'Mirage', 'Tailgater', 'Hippo', 'ICE', 'Eloquent', 'Iron', 'Foxy', 'Judgement', 'Mustang', 'Night', 'Radio', 'Spicy', 'Stone', 'Violin', 'Toxic', 'Vicious', 'Karma', 'Lucky', 'Jersey', 'Cloud']
-        ruAdj = ['Fancy', 'Venomous', 'Turla', 'Energetic', 'Sand', 'Black', 'Epic', 'Buh', 'Xeno', 'Cozy', 'Tele', 'Mini', 'Euro', 'Crouching', 'Anger', 'Koala', 'Dragon', 'Satellite', 'SNAKE', 'Grizzly', 'Tsar', 'Pawn', 'Water', 'Cyber', 'Team', 'So', 'Stront', 'Nobel', 'Grey', 'Cobalt', 'Ban']
-        nkAdj = ['Lazarus', 'Labyrinth', 'Scar', 'Kim', 'Bureau', 'Blue', 'Red', 'Venus', 'Silent', 'Velvet', 'Riccochet', 'Hidden', 'Stardust', 'Inky', 'Apple', 'Onion', 'Cloud']
-        irAdj = ['Rocket', 'Magic', 'Charming', 'Green', 'Oil', 'Slayer', 'Copy', 'Helix', 'Lazy', 'Dark', 'i', 'Twisted', 'Muddy', 'Silent', 'Cobalt', 'Flash', 'Domestic', 'Leaf', 'Boss', 'Seed', 'Yellow', 'Static', 'Elfin', 'POSH', 'Refined', 'Saffron', 'News', 'Fraternal', 'Clever', 'Cunning', 'Volatile', 'Nemesis', 'Remix']
-        natoAdj = ['GOSSIP', 'Equation', 'Grey', 'Snow', 'Project', 'Sea', 'House', 'Rem', 'Animal', 'Regin', 'ETERNAL', 'Longhorn', 'Rattle', 'Tilded', 'Taj', 'Hacking']
+    class Adjective: # organization is important for CTI professionals--even if it is crudely implemented
+        cnAdj = ['Comment', 'Putter', 'Wicked', 'Gothic', 'Emissary', 'Golden', 'Aurora', 'Dynamite', 'Turbine', 'Vixen', 'Sneaky', 'Wet', 
+                 'Pitty', 'Strong', 'Deep', 'Cactus', 'Shadow', 'Royal', 'Mirage', 'Tailgater', 'Hippo', 'ICE', 'Eloquent', 'Iron', 'Foxy', 
+                 'Judgement', 'Mustang', 'Night', 'Radio', 'Spicy', 'Stone', 'Violin', 'Toxic', 'Vicious', 'Karma', 'Lucky', 'Jersey', 'Cloud', 
+                 'Brass', 'Silk', 'Charcoal', 'Nylon', 'Violet']
+        ruAdj = ['Fancy', 'Venomous', 'Turla', 'Energetic', 'Sand', 'Black', 'Epic', 'Buh', 'Xeno', 'Cozy', 'Tele', 'Mini', 'Euro', 'Crouching', 
+                 'Anger', 'Koala', 'Dragon', 'Satellite', 'SNAKE', 'Grizzly', 'Tsar', 'Pawn', 'Water', 'Cyber', 'Team', 'So', 'Stront', 'Nobel', 
+                 'Grey', 'Cobalt', 'Ban', 'Aqua', 'Ghost', 'Seashell', 'Midnight', 'Star']
+        nkAdj = ['Lazarus', 'Labyrinth', 'Scar', 'Kim', 'Bureau', 'Blue', 'Red', 'Venus', 'Silent', 'Velvet', 'Riccochet', 'Hidden', 'Stardust', 
+                 'Inky', 'Apple', 'Onion', 'Cloud', 'Ruby', 'Emerald', 'Diamond', 'H0ly']
+        irAdj = ['Rocket', 'Magic', 'Charming', 'Green', 'Oil', 'Slayer', 'Copy', 'Helix', 'Lazy', 'Dark', 'i', 'Twisted', 'Muddy', 'Silent', 'Cobalt', 
+                 'Flash', 'Domestic', 'Leaf', 'Boss', 'Seed', 'Yellow', 'Static', 'Elfin', 'POSH', 'Refined', 'Saffron', 'News', 'Fraternal', 'Clever', 
+                 'Cunning', 'Volatile', 'Nemesis', 'Remix', 'Pink', 'Peach', 'Mango', 'Pumpkin', 'Vice', 'Moses']
+        natoAdj = ['GOSSIP', 'Equation', 'Grey', 'Snow', 'Project', 'Sea', 'House', 'Rem', 'Animal', 'Regin', 'ETERNAL', 'Longhorn', 'Rattle', 'Tilded', 
+                   'Taj', 'Hacking']
         menaAdj = ['Cheshire', 'Mole', 'Arid', 'Gaza', 'Caliphate', 'Tempting', 'Zoo', 'Big', 'Desert', 'Aluminum']
-        otherAdj = ['El', 'Wild', 'Transparent', 'Mythic', 'Ocean', 'Hellsing', 'Patchwork', 'Gorgon', 'Golden', 'Indrik', 'Wizard', 'Mummy', 'Platinum', 'Evil', 'Wicked', 'Skeleton', 'Pinchy', 'Lunar', 'Grim', 'Patch', 'Bitter', 'Empire', 'Cosmic']
+        otherAdj = ['El', 'Wild', 'Transparent', 'Mythic', 'Ocean', 'Hellsing', 'Patchwork', 'Gorgon', 'Golden', 'Indrik', 'Wizard', 'Mummy', 'Platinum', 
+                    'Evil', 'Wicked', 'Skeleton', 'Pinchy', 'Lunar', 'Grim', 'Patch', 'Bitter', 'Empire', 'Cosmic', 'Spandex', 'Zigzag', 'Sangria', 
+                    'Plaid', 'Tomato', 'Camouflage', 'Periwinkle', 'Pistachio', 'Manatee', 'Click', 'Gand', 'LAP']
         adjList = [cnAdj, ruAdj, nkAdj, irAdj, natoAdj, menaAdj, otherAdj]
 
-    class Noun: # this way we know which general nation-states are associated with our APT group!
+    class Noun: # this way we know which general nation-states are associated with our APT group! This has not held up over time...
         cnNoun = ['Dragon', 'Butler', 'Panda', 'Shop', 'Pity', 'Team', 'Group', 'Dog', 'iom', 'nti', 'Umbrella', 'Gang', 'FOG', 'Fox',
-                'Nightshade', 'Ranger', 'PRESIDENT', 'Cat', 'Mouse', 'Tiger', 'Mimic', 'Traveler', 'Network', 'Atlas', 'Hopper', 'Mikes', 'Wekby']
+                'Nightshade', 'Ranger', 'PRESIDENT', 'Cat', 'Mouse', 'Tiger', 'Mimic', 'Traveler', 'Network', 'Atlas', 'Hopper', 'Mikes', 
+                  'Wekby', 'Typhoon']
         ruNoun = ['Bear', 'Turla', 'Dukes', 'Duke', 'Worm', 'Energy', 'Trap', 'time', 'pak', 'Odin', 'Spider', 'Yeti',
-                'Team', 'Car', 'Bots', 'fly', 'MACKEREL', 'Steppe', 'Storm', 'bug', 'Berkut', 'Spy', 'Crew', 'facy', 'ium']
+                'Team', 'Car', 'Bots', 'fly', 'MACKEREL', 'Steppe', 'Storm', 'bug', 'Berkut', 'Spy', 'Crew', 'facy', 'ium', 'Blizzard']
         nkNoun = ['Chollima', 'Group', 'Reaper', 'Cruft', 'Dog', 'suky', 'Hermit',
-                'noroff', 'Eyes', 'Romanic', 'Cobra', '121', '123', 'Squid', 'Andariel', 'Dragon']
+                'noroff', 'Eyes', 'Romanic', 'Cobra', '121', '123', 'Squid', 'Andariel', 'Dragon', 'Sleet', 'gh0st']
         irNoun = ['Kitten', 'bug', 'of Persia', 'Rig', 'GYPSY', 'Meerkat', 'Hydrus', 'Beanie', 'Water', 'Librarian',
-                'miner', 'SecTeam', 'Ulster', 'Dickens', 'Mermaid', 'Rose', 'CASTER', 'Beef', 'Jackal', 'Chopper']
-        natoNoun = ['GIRL', 'Group', 'Lamberts', 'globe', 'Sauron',
-                    'Turtle', 'sec', 'fly', 'Farm', 'Romance', 'Blue', 'Mahal']
-        menaNoun = ['Viper', 'rats', 'Gang', 'Rat', 'Caracal', 'Jackal',
-                    'Cedar', 'Park', 'Bang', 'Falcon', 'Scorpion', 'SARATOGA', 'WITRE']
+                'miner', 'SecTeam', 'Ulster', 'Dickens', 'Mermaid', 'Rose', 'CASTER', 'Beef', 'Jackal', 'Chopper', 'Sandstorm', 'Leaker', 'wood', 'Staff']
+        natoNoun = ['GIRL', 'Group', 'Lamberts', 'globe', 'Sauron', 'Turtle', 'sec', 'fly', 'Farm', 'Romance', 'Blue', 'Mahal']
+        menaNoun = ['Viper', 'rats', 'Gang', 'Rat', 'Caracal', 'Jackal', 'Cedar', 'Park', 'Bang', 'Falcon', 'Scorpion', 'SARATOGA', 'WITRE', 'Rain', 'Dust']
         otherNoun = ['Machete', 'Neutron', 'Tribe', 'Leopard', 'Lotus', 'Promethium', 'Bahamut',
-                    'Hotel', 'Goblin', 'Corp', 'Spider', 'Oasis', 'Eagle', 'work', 'Monkey', 'Lynx']
+                    'Hotel', 'Goblin', 'Corp', 'Spider', 'Oasis', 'Eagle', 'work', 'Monkey', 'Lynx', 'Hail', 'Cyclone', 'Flood', 'Tsunami', 'Vallhund', 
+                    'Pirate', 'Crab', 'SUS$']
         nounList = [cnNoun, ruNoun, nkNoun, irNoun, natoNoun, menaNoun, otherNoun]
 
     class APT:   
@@ -60,6 +71,8 @@ def makeAPTName():
                     print(final)
                 else:
                     final = ''.join([adj, noun]) # make compound words
+            elif noun.startswith('SUS') or adj.endswith('AP'): # LAPSUS$ makes an edge case, surprise!
+                final = ''.join([adj, noun]) # make compound words
             else: # aka n.isupper():
                 final = ' '.join([adj, noun]) # here's the result if no edge case applies
             return final
